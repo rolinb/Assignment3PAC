8920bcda126160addcb61873d7ccee63bde51214
Mark Hills <markehills66@uvic.ca>
2018-11-07 14:31:29 -0800

    adding basic logintest and updated gradle

--- app/src/androidTest/java/com/example/android/assignment3_pac/LoginTest.java
+++ app/src/androidTest/java/com/example/android/assignment3_pac/LoginTest.java
@@ -1,6 +1,8 @@
 package com.example.android.assignment3_pac;
 
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.rule.ActivityTestRule;
@@ -9,9 +11,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
+import static android.support.test.espresso.intent.Intents.*;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -20,10 +28,12 @@ public class LoginTest {
     @Rule
     public ActivityTestRule<Login> mLoginRule = new ActivityTestRule<>(Login.class);
 
-    @Test
-    public void verifyButtonsExist(){
-        Espresso.onView(withId(R.id.admin_login_button)).perform((click()));
 
+    @Test
+    public void adminLogin(){
+        onView(withId(R.id.admin_login_button)).perform(click());
+
+        onView(withText("AdminHome")).check(matches(isDisplayed()));
 
     }
 
