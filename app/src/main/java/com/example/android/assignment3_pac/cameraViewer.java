2e434a3aaaa60a1d0e5a863d9c00f46fff77418c
Rolin Buckoke <rbuckoke@uvic.ca>
2018-11-11 15:10:00 -0800

    Updated camera to return isRecording, updated camera viewer to actually update camera setting and to display warning if it's full

--- app/src/main/java/com/example/android/assignment3_pac/cameraViewer.java
+++ app/src/main/java/com/example/android/assignment3_pac/cameraViewer.java
@@ -11,9 +11,12 @@ import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.example.android.assignment3_pac.assn2.mainController;
 import com.example.android.assignment3_pac.assn2.part1.devices.Camera;
+import com.example.android.assignment3_pac.assn2.part1.devices.CameraFullException;
 
 import java.util.UUID;
 
@@ -28,15 +31,31 @@ public class cameraViewer extends AppCompatActivity {
 
         Intent intent = getIntent();
         String uuid = intent.getStringExtra("UUID");
-        Camera camera = (Camera) mainController.controller.getDevice(UUID.fromString(uuid));
+        final Camera camera = (Camera) mainController.controller.getDevice(UUID.fromString(uuid));
 
         TextView deviceInfo = findViewById(R.id.DeviceInformation);
         deviceInfo.append(camera.toString() + "\n");
         deviceInfo.append("Status: " + camera.getStatus());
 
+        ToggleButton toggle = findViewById(R.id.Record);
+        if(camera.getIsRecording()) {
+            toggle.setChecked(true);
+        }
 
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    camera.record();
+                }catch(CameraFullException e){
+                    Toast.makeText(cameraViewer.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
+
+
 
 
 
